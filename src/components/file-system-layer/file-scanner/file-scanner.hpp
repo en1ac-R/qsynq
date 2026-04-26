5cacbaea5db6f6c145aee0d76c2c5532a62cd93c
eniac <eniac_r@vk.com>
2026-04-27 01:18:27 +0600

    Реализация FileScanner

---
+++ src/components/file-system-layer/file-scanner/file-scanner.hpp
@@ -0,0 +1,36 @@
+#include <filesystem>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+
+struct FileEntry {
+    std::filesystem::path path;
+    uint64_t size;
+    uint64_t mtime;  // Время последнего изменения в секундах
+
+    explicit FileEntry(std::filesystem::path path, uint64_t size, uint64_t mtime)
+        : path(std::move(path)), size(size), mtime(mtime) {}
+    FileEntry() = default;
+};
+
+// @TODO добавить поддержку IndexDB
+class FileScanner {
+   public:
+    explicit FileScanner(const std::filesystem::path& path,
+                         const std::unordered_set<std::filesystem::path>& ignoredPaths = {});
+    FileScanner(FileScanner&&) = default;
+    FileScanner& operator=(FileScanner&&) = default;
+
+    std::vector<FileEntry> scan();
+
+   private:
+    void scanDirectory(const std::filesystem::path& dirPath,
+                       std::stack<std::filesystem::path>& dirStack,
+                       std::vector<FileEntry>& entries);
+    FileEntry scanFile(const std::filesystem::path& filePath);
+    bool isIgnored(const std::filesystem::path& filePath) const;
+
+   private:
+    std::filesystem::path rootPath_;
+    std::unordered_set<std::filesystem::path> ignoredPaths_;
+};
